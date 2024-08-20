6d60f49d1d404ed7730ddd8cc70210e9e48cfd68
fahmiam2 <fahmiam271@gmail.com>
2024-08-20 15:22:52 +0700

    feat: change start date to 1980-01-01

--- data-engineering/fetch-weather-data.py
+++ data-engineering/fetch-weather-data.py
@@ -21,7 +21,7 @@ url = "https://archive-api.open-meteo.com/v1/archive"
 params = {
 	"latitude": -6.1818,
 	"longitude": 106.8223,
-	"start_date": "2001-01-01",
+	"start_date": "1980-01-01",
 	"end_date": "2023-12-31",
 	"hourly": ["is_day", "sunshine_duration"],
 	"daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "temperature_2m_mean", "apparent_temperature_max", "apparent_temperature_min", "apparent_temperature_mean", "sunrise", "sunset", "daylight_duration", "sunshine_duration", "precipitation_sum", "rain_sum", "snowfall_sum", "precipitation_hours", "wind_speed_10m_max", "wind_gusts_10m_max", "wind_direction_10m_dominant", "shortwave_radiation_sum", "et0_fao_evapotranspiration"],
@@ -106,4 +106,4 @@ daily_data["et0_fao_evapotranspiration"] = daily_et0_fao_evapotranspiration
 daily_dataframe = pd.DataFrame(data = daily_data)
 print(daily_dataframe)
 
-daily_dataframe.to_csv(str(root_directory) + "/data/01_raw/jakarta_daily_weather_data_2000-2023.csv", index=False)
+daily_dataframe.to_csv(str(root_directory) + f"/data/01_raw/jakarta_daily_weather_data_{(params["start_date"])[:4]}-{(params["end_date"])[:4]}.csv", index=False)
